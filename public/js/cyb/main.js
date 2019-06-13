10244e57678033452c6a8df721d1a925a58da917
Papalashvili Dato <papalashvilidato@gmail.com>
2019-06-13 22:34:09 +0400

    new

--- public/js/cyb/main.js
+++ public/js/cyb/main.js
@@ -184,11 +184,13 @@ void function Init() {
             var number = 1
             $('.val').text(number)
             $('#service .inc-dec .inc').on('click', function() {
-                number+=1
-               $(this).siblings('.val').text(number)
-                $('input[name="hours"]').val(number);
-                $('input[name="games"]').val(number);
-                getPrice();
+                if(number < 11){
+                    number+=1
+                    $(this).siblings('.val').text(number)
+                    $('input[name="hours"]').val(number);
+                    $('input[name="games"]').val(number);
+                    getPrice();
+                }
             })
     
             $('#service .inc-dec .dec').on('click', function() {
